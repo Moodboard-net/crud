a2fcdbd1e1bef4183df1ed9eef7a354e8ecadd2b
Moodboard-net <ramadhanw240@gmail.com>
2025-07-11 00:43:00 +0700

    Pertama

---
+++ config/Database.js
@@ -0,0 +1,19 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql'
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+export default db;
---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import fileUpload from 'express-fileupload';
+import ProductRoute from './routes/ProductRoute.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.static('public'));
+app.use(ProductRoute);
+
+app.listen(process.env.PORT, () => console.log('Server up and running'));
